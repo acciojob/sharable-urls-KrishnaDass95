05033fa3e26e17cfd8a164545baac0c5d9345074
Krishna Dass <43197751+KrishnaDass95@users.noreply.github.com>
2023-02-17 11:11:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// your code here
+var nameInput = document.querySelector('#name');
+var yearInput = document.querySelector('#year');
+var submitButton = document.querySelector('#button');
+
+function updateURL(event){
+	event.preventDefault();
+	var name = nameInput.value;
+}
+
+submitButton.addEventListener('click', updateURL);
